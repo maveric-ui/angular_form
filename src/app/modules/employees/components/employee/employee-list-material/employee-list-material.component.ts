f7022a119bc20692a9ab08670385bbe9a6d74750
maveric-ui <maveric-ui@gmail.com>
2018-08-03 15:39:42 +0300

    stable angular table

--- src/app/modules/employees/components/employee/employee-list-material/employee-list-material.component.ts
+++ src/app/modules/employees/components/employee/employee-list-material/employee-list-material.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import {Component, Input, OnInit, ViewChild, ViewEncapsulation, OnChanges, SimpleChanges} from '@angular/core';
 import { Employee } from '../../../classes/employee';
 import { MatDialog, MatSort, MatTableDataSource, Sort } from '@angular/material';
 import { EmployeeReactiveFormComponent } from '../employee-reactive-form/employee-reactive-form.component';
@@ -10,17 +10,24 @@ import { EmployeeReactiveFormComponent } from '../employee-reactive-form/employe
   styleUrls: ['./employee-list-material.component.less'],
   encapsulation: ViewEncapsulation.None
 })
-export class EmployeeListMaterialComponent implements OnInit {
+export class EmployeeListMaterialComponent implements OnInit, OnChanges  {
 
-  @Input('employeeList') public employeeList: Employee[];
+  @Input() public employeeList: Employee[];
+  public soursData;
   @ViewChild(MatSort) sort: MatSort;
-  displayedColumns: string[] = ['id', 'name', 'dateOfBirth', 'hireDate', 'address', 'city', 'country'];
+  displayedColumns: string[] = ['id', 'name', 'position', 'dateOfBirth', 'hireDate', 'address', 'city', 'country'];
 
   constructor(public dialog: MatDialog) { }
 
   ngOnInit() {
-    // this.employeeList.sort = this.sort;
   }
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['employeeList']) {
+      this.soursData = new MatTableDataSource(this.employeeList);
+      this.soursData.sort = this.sort;
+    }
+  }
+
 
   public openNewEmployeeFrom() {
     this.dialog.open(EmployeeReactiveFormComponent, {autoFocus: false});
