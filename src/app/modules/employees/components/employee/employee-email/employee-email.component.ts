a2944267fec6f4b1112014f5a70e6e0cb26a1630
maveric-ui <lokhmanov.maksym@gmail.com>
2018-08-08 23:13:09 +0300

    upd

---
+++ src/app/modules/employees/components/employee/employee-email/employee-email.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-email',
+  templateUrl: './employee-email.component.html',
+  styleUrls: ['./employee-email.component.less']
+})
+export class EmployeeEmailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/modules/employees/components/employee/employee-singin-form/employee-singin-form.component.ts
+++ src/app/modules/employees/components/employee/employee-singin-form/employee-singin-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { MatDialogRef } from '@angular/material';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-employee-singin-form',
@@ -9,11 +10,46 @@ import { MatDialogRef } from '@angular/material';
 })
 export class EmployeeSinginFormComponent implements OnInit {
 
-  constructor(private matDialogRef: MatDialogRef<EmployeeSinginFormComponent>) { }
+  public signInForm: FormGroup;
+  public errorMessages = {
+    required: 'This field is required'
+  };
+
+  public get name() { return this.signInForm.get('name'); }
+  public get password() { return this.signInForm.get('password'); }
+
+  constructor(private matDialogRef: MatDialogRef<EmployeeSinginFormComponent>, private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.initSingInForm();
   }
 
+  private initSingInForm() {
+    this.signInForm = this.fb.group({
+      name: [null, [
+        Validators.required
+      ]],
+      password: [null, [
+        Validators.required
+      ]]
+    });
+  }
+
+  onSingIn(user) {
+    const controls = this.signInForm.controls;
+
+    if (!this.signInForm.valid) {
+      for (const control in controls) {
+        if (this.signInForm.get(control)) {
+          this.signInForm.get(control).markAsTouched({onlySelf: true});
+          this.signInForm.get(control).markAsDirty({onlySelf: true});
+        }
+      }
+      return;
+    }
+
+    console.log(user);
+  }
 
   close() {
     this.matDialogRef.close();
